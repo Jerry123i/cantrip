22ca17f0ae44aeacada5634aecdf26213902d356
João Guio <joaoluisguio@gmail.com>
2017-07-25 17:22:41 -0300

    v0.0.7
    Added extra room command, but not fully working.

--- Assets/Scripts/Level Generator/LevelModel.cs
+++ Assets/Scripts/Level Generator/LevelModel.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 public class LevelModel : ScriptableObject {
 
 
-    private const int mainRoomsSizeMin = 5, mainRoomsSizeMax = 10, levelSizeMax = 10;
+    private const int mainRoomsSizeMin = 9, mainRoomsSizeMax = 15, levelSizeMax = 15, extraRoomsSizeMin = 3, extraRoomsSizeMax = 8;
     private Vector3 initialPoint;
 
     public int mainRoomsSize, pathRoomsSize, extraRoomsSize, fi, fj;
     public int[,] levelMatrix;
 	public RoomModel[] mainRooms;
+    public RoomModel[] pathRooms;
+    public RoomModel[] extraRooms;
 
     private readonly System.Random rnd = new System.Random();
 
@@ -20,6 +22,7 @@ public class LevelModel : ScriptableObject {
         MAIN = 1,
         PATH = 2,
         EXTRA = 3,
+        FINAL = 4
     }
 
     private enum DIR
@@ -50,105 +53,232 @@ public class LevelModel : ScriptableObject {
         
     }
 
-    private void CreateMainRooms(Transform parent) {
+    private void PrintMatrix(int[,] matrix, int n, int m) {
+        string line;
+        for (int i = 0; i < n; i++) 
+        {
+            line = "";
+            for (int j = 0; j < m; j++) {
+                line += String.Format("{0} ", matrix[i,j]);
+            }    
+            Debug.Log(line);
+        }
+    }
+
+    private int FindEmptyDir(int dir) {
+		bool retry = false;
+        int newDir = dir;
+		while (fi < 0 || fj < 0 || fi > levelSizeMax - 1 || fj > levelSizeMax - 1 || levelMatrix[fi, fj] != 0)
+		{
+			if (retry)
+			{
+				if (newDir == 0) fj--;
+				if (newDir == 1) fi--;
+				if (newDir == 2) fj++;
+				if (newDir == 3) fi++;
+			}
+			newDir = rnd.Next(0, 4);
+			if (newDir == 0) fj++;
+			if (newDir == 1) fi++;
+			if (newDir == 2) fj--;
+			if (newDir == 3) fi--;
+			retry = true;
+		}
+        return newDir;
+    }
+
+    private RoomModel FindRoomFromIndexes(int indexI, int indexJ) {
+        for (int i = 0; i < mainRoomsSize; i++)
+        {
+            if (mainRooms[i].Pointi == indexI && mainRooms[i].Pointj == indexJ)
+                return mainRooms[i];
+        }
+        return null;
+    }
+
+    private void CreateMainRooms()
+    {
         Vector3 previous = new Vector3(-1.0f, -1.0f, -1.0f);
-        GameObject room;
-        int rand, dir = 0, lastDir = -1;
-        bool retry = false;
+        //GameObject room;
+        int dir = 0, lastDir = -1;
         initialPoint = new Vector3(0.0f, 0.0f, 0.0f);
 
         for (int i = 0; i < mainRoomsSize; i++)
         {
-            levelMatrix[fi, fj] = (int)LEVEL.MAIN;
-
             mainRooms[i] = ScriptableObject.CreateInstance("RoomModel") as RoomModel;
-            room = Instantiate(new GameObject());
-            room.name = "room " + i;
-            room.transform.parent = parent;
 
-            // Distributing possibilites
-            retry = false;
-            while (fi < 0 || fj < 0 || fi > levelSizeMax - 1 || fj > levelSizeMax - 1 || levelMatrix[fi, fj] != 0)
-            {
-                if (retry) {
-	                if (dir == 0) fj--;
-	                if (dir == 1) fi--;
-	                if (dir == 2) fj++;
-	                if (dir == 3) fi++;
-                }
-                rand = 0;
-                dir = rnd.Next(0, 4);
-                if (dir == 0) fj++;
-                if (dir == 1) fi++;
-                if (dir == 2) fj--;
-                if (dir == 3) fi--;
-                retry = true;
-            } 
+            if (i != mainRoomsSize - 1)
+                levelMatrix[fi, fj] = (int)LEVEL.MAIN;
+            else
+                levelMatrix[fi, fj] = (int)LEVEL.FINAL;
 
-            mainRooms[i].size[0] = rnd.Next(RoomModel.minSize[0], RoomModel.maxSize[0]);
-            mainRooms[i].size[1] = rnd.Next(RoomModel.minSize[1], RoomModel.maxSize[1]);
+            mainRooms[i].Pointi = fi;
+            mainRooms[i].Pointj = fj;
+
+            mainRooms[i].Size[0] = rnd.Next(RoomModel.minSize[0], RoomModel.maxSize[0]);
+            mainRooms[i].Size[1] = rnd.Next(RoomModel.minSize[1], RoomModel.maxSize[1]);
+
+            dir = FindEmptyDir(dir);
+
+            mainRooms[i].ExitDirs.Add(dir);
+            if (i != 0) mainRooms[i].EnterDirs.Add(lastDir);
 
             if (i != mainRoomsSize - 1)
             {
-                mainRooms[i].nextRoomPoint = new Vector3(
-                    (dir == (int)DIR.LEFT) ? 0 : (dir == (int)DIR.RIGHT) ? mainRooms[i].size[0] - 1 : rnd.Next(1, RoomModel.minSize[0] - 1),
-                    (dir == (int)DIR.DOWN) ? 0 : (dir == (int)DIR.UP) ? mainRooms[i].size[1] - 1 : rnd.Next(1, RoomModel.minSize[1] - 1),
+                mainRooms[i].nextRoomPoints.Add(new Vector3(
+                    (dir == (int)DIR.LEFT) ? 0 : (dir == (int)DIR.RIGHT) ? mainRooms[i].Size[0] - 1 : rnd.Next(1, RoomModel.minSize[0] - 1),
+                    (dir == (int)DIR.DOWN) ? 0 : (dir == (int)DIR.UP) ? mainRooms[i].Size[1] - 1 : rnd.Next(1, RoomModel.minSize[1] - 1),
                     0.0f
-                );
+                ));
             }
-            else 
+            else
             {
-                mainRooms[i].nextRoomPoint = new Vector3(-1.0f, -1.0f, -1.0f);
+                mainRooms[i].nextRoomPoints.Add(new Vector3(-1.0f, -1.0f, -1.0f));
             }
 
-            if (i != 0) {
-	            initialPoint += new Vector3(
-                    (lastDir == (int)DIR.LEFT) ? -1 - RoomModel.maxSize[0] : (lastDir == (int)DIR.RIGHT) ? 1 + RoomModel.maxSize[0] : 0,
-                    (lastDir == (int)DIR.DOWN) ? -1 - RoomModel.maxSize[1] : (lastDir == (int)DIR.UP) ? 1 + RoomModel.maxSize[1] : 0,
-	                0.0f
-	            );
-            } 
-            else {
-                initialPoint = new Vector3(0.0f, 0.0f, 0.0f);
-            
-            }
-
-			mainRooms[i].initialPoint = initialPoint;
-
-            if (lastDir == (int)DIR.LEFT || lastDir == (int)DIR.DOWN) {
-                previous = new Vector3(
-                    (lastDir == (int)DIR.LEFT) ? mainRooms[i].size[0] - 1 : previous.x,
-                    (lastDir == (int)DIR.DOWN) ? mainRooms[i].size[1] - 1 : previous.y, 
-                    0.0f
-                );
-			}
-			mainRooms[i].previousRoomPoint = previous;
-            previous = new Vector3(
-                (dir == (int)DIR.RIGHT) ? 0 : mainRooms[i].nextRoomPoint.x,
-                (dir == (int)DIR.UP) ? 0 : mainRooms[i].nextRoomPoint.y,
+            initialPoint = new Vector3(
+                RoomModel.maxSize[0] * mainRooms[i].Pointi,
+                RoomModel.maxSize[1] * mainRooms[i].Pointj,
                 0.0f
             );
+            mainRooms[i].initialPoint = initialPoint;
 
-			mainRooms[i].Init(room.transform);
-            if (i != 0)
+            if (lastDir == (int)DIR.LEFT || lastDir == (int)DIR.DOWN)
             {
-				if (lastDir == (int)DIR.RIGHT || lastDir == (int) DIR.LEFT)
-                {
-                    mainRooms[i - 1].CreatePathBetween(room.transform, RoomModel.maxSize[0] - ((lastDir == (int)DIR.LEFT) ? mainRooms[i].size[0] : mainRooms[i - 1].size[0]), lastDir);
-                }
-                else
-                {
-                    mainRooms[i - 1].CreatePathBetween(room.transform, RoomModel.maxSize[1] - ((lastDir == (int)DIR.DOWN) ? mainRooms[i].size[1] : mainRooms[i - 1].size[1]), lastDir);
-                }
+                previous = new Vector3(
+                    (lastDir == (int)DIR.LEFT) ? mainRooms[i].Size[0] - 1 : previous.x,
+                    (lastDir == (int)DIR.DOWN) ? mainRooms[i].Size[1] - 1 : previous.y,
+                    0.0f
+                );
             }
+            mainRooms[i].previousRoomPoints.Add(previous);
+            previous = new Vector3(
+                (dir == (int)DIR.RIGHT) ? 0 : mainRooms[i].nextRoomPoints[mainRooms[i].nextRoomPoints.Count - 1].x,
+                (dir == (int)DIR.UP) ? 0 : mainRooms[i].nextRoomPoints[mainRooms[i].nextRoomPoints.Count - 1].y,
+                0.0f
+            );
             lastDir = dir;
-		}
+
+        }
     }
 
-	private void CreatePathRooms(Transform parent)
-	{
 
+    private void CreateExtraRooms()
+    {
+        Vector3 previous = new Vector3(-1.0f, -1.0f, -1.0f);
+        List<List<int>> possibles = new List<List<int>>();
+        int dir = 0;
+        RoomModel mainRoom;
+
+        for (int j = 0; j < levelSizeMax; j++)
+        {
+            for (int k = 0; k < levelSizeMax; k++)
+            {
+                if (levelMatrix[j, k] == (int)LEVEL.MAIN)
+                {
+                    List<int> n = new List<int>();
+                    n.Add(j); n.Add(k);
+                    possibles.Add(n);
+                }
+            }
+        }
+
+        extraRoomsSize = rnd.Next(extraRoomsSizeMin, possibles.Count);
+		extraRooms = new RoomModel[extraRoomsSize];
+
+        for (int i = 0; i < possibles.Count && i < extraRoomsSize; i++)
+        {
+            extraRooms[i] = ScriptableObject.CreateInstance("RoomModel") as RoomModel;
+
+            fi = possibles[i][0];
+            fj = possibles[i][1];
+			dir = FindEmptyDir(dir);
+			//Debug.Log("index i: " + possibles[i][0] + " - index j: " + possibles[i][1]);
+			mainRoom = FindRoomFromIndexes(possibles[i][0], possibles[i][1]);
+			if (mainRoom != null)
+            {
+                mainRoom.ExitDirs.Add(dir);
+                extraRooms[i].EnterDirs.Add(dir);
+
+                mainRoom.nextRoomPoints.Add(new Vector3(
+                        (dir == (int)DIR.LEFT) ? 0 : (dir == (int)DIR.RIGHT) ? mainRooms[i].Size[0] - 1 : rnd.Next(1, RoomModel.minSize[0] - 1),
+                        (dir == (int)DIR.DOWN) ? 0 : (dir == (int)DIR.UP) ? mainRooms[i].Size[1] - 1 : rnd.Next(1, RoomModel.minSize[1] - 1),
+                        0.0f
+                    ));
+
+                levelMatrix[fi, fj] = (int)LEVEL.EXTRA;
+                extraRooms[i].Pointi = fi;
+                extraRooms[i].Pointj = fj;
+                //Debug.Log("fi: " + fi + " - fj: " + fj);
+
+                extraRooms[i].Size[0] = rnd.Next(RoomModel.minSize[0], RoomModel.maxSize[0]);
+                extraRooms[i].Size[1] = rnd.Next(RoomModel.minSize[1], RoomModel.maxSize[1]);
+
+                //extraRooms[i].nextRoomPoint = new Vector3(-1.0f, -1.0f, -1.0f);
+
+                initialPoint = new Vector3(
+                    RoomModel.maxSize[0] * extraRooms[i].Pointi,
+                    RoomModel.maxSize[1] * extraRooms[i].Pointj,
+                    0.0f
+                );
+                extraRooms[i].initialPoint = initialPoint;
+
+                extraRooms[i].previousRoomPoints.Add(new Vector3(
+                    (mainRoom.ExitDirs[mainRoom.ExitDirs.Count - 1] == (int)DIR.LEFT) ? mainRoom.Size[0] - 1 : (mainRoom.ExitDirs[mainRoom.ExitDirs.Count - 1] == (int)DIR.RIGHT) ? 0 : mainRoom.nextRoomPoints[mainRoom.nextRoomPoints.Count - 1].x,
+                    (mainRoom.ExitDirs[mainRoom.ExitDirs.Count - 1] == (int)DIR.DOWN) ? mainRoom.Size[1] - 1 : (mainRoom.ExitDirs[mainRoom.ExitDirs.Count - 1] == (int)DIR.UP) ? 0 : mainRoom.nextRoomPoints[mainRoom.nextRoomPoints.Count - 1].y,
+                    0.0f
+                ));
+                extraRooms[i].roomFrom = mainRoom;
+                //Debug.Log(extraRooms[i].previousRoomPoints[0].ToString());
+
+            }
+            else {
+                Debug.Log("Main room not found.");
+            }
+        }
+    }
+
+    public void InstantiateMainRooms(Transform parent) {
+        for (int i = 0; i < mainRoomsSize; i++) {
+            GameObject room = Instantiate(new GameObject());
+            room.name = "mainRoom " + i;
+            room.transform.parent = parent;
+			mainRooms[i].Init(room.transform);
+
+		}
+        for (int i = 1; i < mainRoomsSize; i++) {
+            if (mainRooms[i].EnterDirs[0] == (int)DIR.RIGHT || mainRooms[i].EnterDirs[0] == (int)DIR.LEFT)
+            {
+                mainRooms[i - 1].CreatePathTo(RoomModel.maxSize[0] - ((mainRooms[i].EnterDirs[0] == (int)DIR.LEFT) ? mainRooms[i].Size[0] : mainRooms[i - 1].Size[0]) - 1, mainRooms[i].EnterDirs[0], mainRooms[i - 1].nextRoomPoints[0]);
+            }
+            else {
+                mainRooms[i - 1].CreatePathTo(RoomModel.maxSize[1] - ((mainRooms[i].EnterDirs[0] == (int)DIR.DOWN) ? mainRooms[i].Size[1] : mainRooms[i - 1].Size[1]) - 1, mainRooms[i].EnterDirs[0], mainRooms[i - 1].nextRoomPoints[0]);
+            }
+        }
 	}
+
+
+    public void InstantiateExtraRooms(Transform parent) {
+        for (int i = 0; i < extraRoomsSize; i++) {
+            GameObject room = Instantiate(new GameObject());
+            room.name = "extraRoom " + i;
+            room.transform.parent = parent;
+			extraRooms[i].Init(room.transform);
+		}
+        for (int i = 0; i < extraRoomsSize; i++) {
+            if (extraRooms[i].EnterDirs[0] == (int)DIR.RIGHT || extraRooms[i].EnterDirs[0] == (int)DIR.LEFT)
+            {
+                extraRooms[i].roomFrom.CreatePathTo(RoomModel.maxSize[0] - ((extraRooms[i].EnterDirs[0] == (int)DIR.LEFT) ? extraRooms[i].Size[0] : extraRooms[i].roomFrom.Size[0]) - 1, extraRooms[i].EnterDirs[0], extraRooms[i].previousRoomPoints[0]);
+            }
+            else
+            {
+                extraRooms[i].roomFrom.CreatePathTo(RoomModel.maxSize[1] - ((extraRooms[i].EnterDirs[0] == (int)DIR.DOWN) ? extraRooms[i].Size[1] : extraRooms[i].roomFrom.Size[1]) - 1, extraRooms[i].EnterDirs[0], extraRooms[i].previousRoomPoints[0]);
+				//extraRooms[i].CreatePathTo(RoomModel.maxSize[1] - ((dir == (int)DIR.DOWN) ? extraRooms[i].Size[1] : mainRooms[i].Size[1]), dir);
+			}
+        }
+	}
+
+
 
 	private void CreateExtraRooms(Transform parent)
 	{
@@ -162,9 +292,13 @@ public class LevelModel : ScriptableObject {
 		mainRooms = new RoomModel[mainRoomsSize];
         levelMatrix = new int[levelSizeMax,levelSizeMax];
         fi = mainRoomsSize / 2;
-        fj = mainRoomsSize / 2;
-        CreateMainRooms(parent);
-        CreatePathRooms(parent);
-        CreateExtraRooms(parent);
+		fj = mainRoomsSize / 2;
+
+        CreateMainRooms();
+        CreateExtraRooms();
+        InstantiateMainRooms(parent);
+        InstantiateExtraRooms(parent);
+
+		CreateExtraRooms(parent);
     }
 }
--- Assets/Scripts/Level Generator/RoomModel.cs
+++ Assets/Scripts/Level Generator/RoomModel.cs
@@ -15,10 +15,72 @@ public class RoomModel : ScriptableObject
     private GameObject exit;
     private GameObject[] path;
 
-    public int[] size;
+    private bool isFinal;
+    private int[] size;
+    private List<int> exitDirs, enterDirs;
     public Vector3 initialPoint;
-    public Vector3 previousRoomPoint;
-    public Vector3 nextRoomPoint;
+    public List<Vector3> previousRoomPoints;
+    public List<Vector3> nextRoomPoints;
+
+    private int pointi, pointj;
+
+    public RoomModel roomFrom;
+
+    #region Getter and Setters
+    public int Pointi {
+        get {
+            return pointi;
+        }
+        set {
+            pointi = value; 
+        }
+    }
+
+    public int Pointj {
+        get {
+            return pointj;
+        }
+        set {
+            pointj = value; 
+        }
+    }
+
+    public int[] Size {
+        get {
+            return size;
+        }
+        set {
+            size = value;
+        }
+    }
+
+    public List<int> ExitDirs {
+        get {
+            return exitDirs;
+        }
+        set {
+            exitDirs = value;
+        }
+    }
+
+    public List<int> EnterDirs {
+        get {
+            return enterDirs;
+        }
+        set {
+            enterDirs = value;
+        }
+    }
+
+    public bool IsFinal {
+        get {
+            return isFinal;
+        }
+        set {
+            isFinal = value;
+        }
+    }
+    #endregion
 
     // Magic number that gives the exact distance to put the exit.
     private float distancePath = 0.773f;
@@ -31,7 +93,7 @@ public class RoomModel : ScriptableObject
 		DOWNRIGHT = 3,
 		LEFT = 4,
 		PATHHORIZONTAL = 5,
-		PATHHORIZONTALBLOCKED = 7,
+		PATHHORIZONTALBLOCKED = 6,
         PATHVERTICAL = 8,
         PATHVERTICALBLOCKED = 9,
 		RIGHT = 10,
@@ -44,6 +106,10 @@ public class RoomModel : ScriptableObject
 	{
 		floorTypes = Resources.LoadAll<GameObject>("Prefabs/Floor");
         size = new int[2];
+        exitDirs = new List<int>();
+        enterDirs = new List<int>();
+		previousRoomPoints = new List<Vector3>();
+        nextRoomPoints = new List<Vector3>();
 	}
 
 	void OnDestroy()
@@ -63,6 +129,7 @@ public class RoomModel : ScriptableObject
 	public void Init(Transform parent)
 	{
         int point;
+        bool notBorder;
 		if (size[1] > maxSize[1] || size[1] > maxSize[1] || size[0] < minSize[0] || size[1] < minSize[1])
 		{
 			Debug.Log("destroy here because it's bigger than max size or smaller than minsize");
@@ -89,92 +156,99 @@ public class RoomModel : ScriptableObject
                     else
                     {
                         //Debug.Log(i + " - " + (int)previousRoomPoint[0] + " - " + j + " - " + (int)previousRoomPoint[1]);
-                        if (i == (int)previousRoomPoint[0] && j == (int)previousRoomPoint[1])
+                        notBorder = false;
+                        for (int k = 0; k < previousRoomPoints.Count; k++)
                         {
-                            floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.BASE], pos, Quaternion.identity);
-                        }
-                        else {
-                            if (i == (int)nextRoomPoint[0] && j == (int)nextRoomPoint[1])
+                            if (i == (int)previousRoomPoints[k][0] && j == (int)previousRoomPoints[k][1])
                             {
-                                if (i > 0 && i < size[0] - 1)
-                                {
-                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.BASE], pos, Quaternion.identity);
-                                    exit = Instantiate(floorTypes[(int)FLOORTYPES.PATHVERTICALBLOCKED], pos + new Vector3(0, (j == 0) ? -1.229f : 0.773f, 0), Quaternion.identity);
-                                    exit.transform.parent = parent;
-                                }
-                                else if (j > 0 && j < size[1] - 1)
-                                {
-                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.BASE], pos, Quaternion.identity);
-                                    exit = Instantiate(floorTypes[(int)FLOORTYPES.PATHHORIZONTALBLOCKED], pos + new Vector3((i == 0) ? -1.224f : 0.773f, 0, 0), Quaternion.identity);
-                                    exit.transform.parent = parent;
-                                }
-                                else
-                                {
-                                    Debug.Log("error, invalid position to path");
-                                }
+								Debug.Log(previousRoomPoints[k].ToString());
+								notBorder = true;
+                                floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.BASE], pos, Quaternion.identity);
                             }
-                            else
+                        }
+                        for (int k = 0; k < nextRoomPoints.Count; k++) {
+							if (i == (int)nextRoomPoints[k][0] && j == (int)nextRoomPoints[k][1])
+							{
+								notBorder = true;
+								if (i > 0 && i < size[0] - 1)
+								{
+									floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.BASE], pos, Quaternion.identity);
+									exit = Instantiate(floorTypes[(int)FLOORTYPES.PATHVERTICALBLOCKED], pos + new Vector3(0, (j == 0) ? -1.229f : 0.773f, 0), Quaternion.identity);
+									exit.transform.parent = parent;
+								}
+								else if (j > 0 && j < size[1] - 1)
+								{
+									floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.BASE], pos, Quaternion.identity);
+									exit = Instantiate(floorTypes[(int)FLOORTYPES.PATHHORIZONTALBLOCKED], pos + new Vector3((i == 0) ? -1.0f : 1.0f, 0, 0), Quaternion.identity);
+									exit.transform.parent = parent;
+								}
+								else
+								{
+									Debug.Log("error, invalid position to path");
+								}
+							}
+                        }
+                        // CREATING BORDERS
+                        if (!notBorder)
+                        {
+                            if (i == 0)
                             {
-                                // CREATING BORDERS
-                                if (i == 0)
+                                if (j == 0)
                                 {
-                                    if (j == 0)
-                                    {
-                                        floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.DOWNLEFT], pos, Quaternion.identity);
-                                    }
-                                    else if (j == size[1] - 1)
-                                    {
-                                        floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.UPLEFT], pos, Quaternion.identity);
-                                    }
-                                    else
-                                    {
-                                        floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.LEFT], pos, Quaternion.identity);
-                                    }
-                                }
-                                else if (i == size[0] - 1)
-                                {
-                                    if (j == 0)
-                                    {
-                                        floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.DOWNRIGHT], pos, Quaternion.identity);
-                                    }
-                                    else if (j == size[1] - 1)
-                                    {
-                                        floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.UPRIGHT], pos, Quaternion.identity);
-                                    }
-                                    else
-                                    {
-                                        floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.RIGHT], pos, Quaternion.identity);
-                                    }
-                                }
-                                else if (j == 0)
-                                {
-                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.DOWN], pos, Quaternion.identity);
+                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.DOWNLEFT], pos, Quaternion.identity);
                                 }
                                 else if (j == size[1] - 1)
                                 {
-                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.UP], pos, Quaternion.identity);
+                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.UPLEFT], pos, Quaternion.identity);
+                                }
+                                else
+                                {
+                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.LEFT], pos, Quaternion.identity);
                                 }
                             }
+                            else if (i == size[0] - 1)
+                            {
+                                if (j == 0)
+                                {
+                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.DOWNRIGHT], pos, Quaternion.identity);
+                                }
+                                else if (j == size[1] - 1)
+                                {
+                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.UPRIGHT], pos, Quaternion.identity);
+                                }
+                                else
+                                {
+                                    floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.RIGHT], pos, Quaternion.identity);
+                                }
+                            }
+                            else if (j == 0)
+                            {
+                                floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.DOWN], pos, Quaternion.identity);
+                            }
+                            else if (j == size[1] - 1)
+                            {
+                                floor[point] = Instantiate(floorTypes[(int)FLOORTYPES.UP], pos, Quaternion.identity);
+                            }
                         }
-					}
+                    }
 					floor[point].transform.parent = parent;
                 }
 			}
 		}
     }
 
-    public void CreatePathBetween(Transform parent, int count, int dir) {
+    public void CreatePathTo(int count, int dir, Vector3 nextRoomPoint) {
         path = new GameObject[count];
         for (int i = 0; i < count; i++)
         {
             if (dir == 0 || dir == 2)
             {
-                path[i] = Instantiate(floorTypes[(int)FLOORTYPES.PATHVERTICAL], initialPoint + new Vector3(nextRoomPoint.x, (dir == 0) ? 1.0f * (i + 1) + size[1] : -1.0f * (i + 2), 0.0f), Quaternion.identity);
+                path[i] = Instantiate(floorTypes[(int)FLOORTYPES.PATHVERTICAL], initialPoint + new Vector3(nextRoomPoint.x, (dir == 0) ? 1.0f * (i + 1) + size[1] - 0.23f : -1.0f * (i + 2) - 0.23f, 0.0f), Quaternion.identity);
             }
             else {
-                path[i] = Instantiate(floorTypes[(int)FLOORTYPES.PATHHORIZONTAL], initialPoint + new Vector3((dir == 1) ? 1.0f * (i + 1) + size[0] : -1.0f * (i + 2), nextRoomPoint.y, 0.0f), Quaternion.identity);
+                path[i] = Instantiate(floorTypes[(int)FLOORTYPES.PATHHORIZONTAL], initialPoint + new Vector3((dir == 1) ? 1.0f * (i + 1) + size[0]: -1.0f * (i + 2), nextRoomPoint.y, 0.0f), Quaternion.identity);
 			}
-            path[i].transform.parent = parent;
+            path[i].transform.parent = floor[0].transform.parent;
         }
     }
 }
