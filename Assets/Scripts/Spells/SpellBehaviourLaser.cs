d7a61501c1cdfc1ead5a3c753e24cf08d4c71bfe
Jerry123i <igorsc.jerry@gmail.com>
2017-08-30 15:02:59 -0300

    Upzinho rápido de quarta-feira
    Fiz a base do laser dps do almoço

---
+++ Assets/Scripts/Spells/SpellBehaviourLaser.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBehaviourLaser : MonoBehaviour {
+
+    public SpellSatistics stats;
+    
+    public float clock;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Extensão do raio
+
+	}
+        
+    void OnTriggerEnter2D(Collider2D cool)
+    {
+        if(cool.GetComponent<EnemyController>() != null)
+        {
+            EnemyController hit;
+            hit = cool.GetComponent<EnemyController>();
+            
+            //Slow
+            if(stats.slowDuration>0 && stats.slowPower > 0)
+            {
+                if (hit.currentEffects.slow > 0)
+                {
+                    hit.CallStopSlow();
+                }
+                hit.currentEffects.slow = stats.slowPower;
+                hit.PullSlow(float.MaxValue);
+            }
+
+            //Poison
+            if(stats.poisonDuration>0 && stats.poisonPower > 0)
+            {
+                if(hit.currentEffects.poison > 0)
+                {
+                    hit.CallStopPoison();
+                }
+                hit.currentEffects.poison = stats.poisonPower;
+                hit.PullPoison(float.MaxValue);
+            }
+
+            //Snare (Vai ter que ter alguma forma de coldown)
+            /*if (stats.snareDuration>0)
+            {
+                if (hit.debuffSnare)
+                {
+                    hit.StopCoroutine("RoutineSnare");
+                }
+                StartCoroutine(hit.RoutineSnare(stats.snareDuration));
+            }*/
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.GetComponent<EnemyController>() != null)
+        {
+            EnemyController hit;
+            hit = coll.GetComponent<EnemyController>();
+
+            //Verificar timing de armadura e efeitos de dano over time
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.GetComponent<EnemyController>() != null)
+        {
+            EnemyController hit;
+            hit = coll.GetComponent<EnemyController>();
+
+            //Slow
+            if (stats.slowDuration > 0 && stats.slowPower > 0)
+            {
+                if (hit.currentEffects.slow > 0)
+                {
+                    hit.CallStopSlow();
+                }
+                hit.currentEffects.slow = stats.slowPower;
+                hit.PullSlow(stats.slowDuration);
+            }
+
+            //Poison
+            if (stats.poisonDuration > 0 && stats.poisonPower > 0)
+            {
+                if (hit.currentEffects.poison > 0)
+                {
+                    hit.CallStopPoison();
+                }
+                hit.currentEffects.poison = stats.poisonPower;
+                hit.PullPoison(stats.poisonDuration);
+            }
+
+            //Snare (Vai ter que ter alguma forma de coldown)
+            /*if (stats.snareDuration>0)
+            {
+                if (hit.debuffSnare)
+                {
+                    hit.StopCoroutine("RoutineSnare");
+                }
+                StartCoroutine(hit.RoutineSnare(stats.snareDuration));
+            }*/
+
+        }
+
+    }
+
+}
