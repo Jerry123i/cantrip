b149c45b0a1a89fa48e40039603159d4405cd903
juliorkm <julioramakrsnamandoju@gmail.com>
2017-09-08 22:55:45 -0300

    CustomEditor convertido para Window
    - Window criada para modificar uma Map Cell (acessável pelo Inspector);
    - Diversas melhorias na apresentação do editor de células;
    - Blueprints possuem scroll horizontal e vertical.

--- Assets/_Serializable/MapCellData.cs
+++ Assets/_Serializable/MapCellData.cs
@@ -33,9 +33,53 @@ public class MapCellData : ScriptableObject {
     }
 }
 
+
 [CustomEditor(typeof(MapCellData))]
 public class MapCellDataCustomEditor : Editor {
-	class Colors {
+    public override void OnInspectorGUI() {
+        MapCellData mcd = (MapCellData)target;
+
+        GUILayout.Label("Cell Name: " + mcd.name);
+        GUILayout.Space(8);
+        GUILayout.Label("Dimensões: " + mcd.dimension_x + "x" + mcd.dimension_y);
+        GUILayout.Space(8);
+        GUILayout.Label("Limites: ");
+        //Centralizar norte/sul
+        GUIStyle centerStyle = new GUIStyle();
+        centerStyle.margin = new RectOffset(57, 0, 0, 0);
+
+        //Serializacao dos Limites
+        GUILayout.BeginVertical("Box");
+        GUILayout.BeginVertical(centerStyle, GUILayout.Width(100));
+        GUILayout.Label(mcd.limites[0].ToString());
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical(GUILayout.Width(100));
+        GUILayout.BeginHorizontal();
+        GUILayout.Label(mcd.limites[1].ToString(), GUILayout.Width(100));
+        GUILayout.Label(mcd.limites[2].ToString(), GUILayout.Width(70));
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+        GUILayout.BeginVertical(centerStyle, GUILayout.Width(100));
+        GUILayout.Label(mcd.limites[3].ToString());
+        GUILayout.EndVertical();
+        GUILayout.EndVertical();
+
+        GUILayout.Space(8);
+        GUILayout.BeginVertical(GUILayout.Width(Screen.width / 2));
+        if (GUILayout.Button(new GUIContent("Open in Map Cell Editor", "Opens the Map Cell editor window for this cell."), GUILayout.Height(50))) {
+            MapCellDataWindow.ShowWindow();
+        }
+        GUILayout.EndVertical();
+    }
+}
+
+public class MapCellDataWindow : EditorWindow {
+    [MenuItem("Window/Map Cell Editor", priority = 1)]
+    public static void ShowWindow() {
+        EditorWindow window = EditorWindow.GetWindow(typeof(MapCellDataWindow));
+        window.titleContent = new GUIContent("Map Cell Editor");
+    }
+    class Colors {
 		public Color vazio;
 		public Color chao;
 		public Color parede;
@@ -53,9 +97,11 @@ public class MapCellDataCustomEditor : Editor {
 		}
 	}
 
+    Vector2 scrollPos;
 
 	private GUIStyle guiStyleColor;
 	private Colors colors;
+    MapCellData mcd;
 
     private int new_x = 0, new_y = 0;
 
@@ -146,42 +192,31 @@ public class MapCellDataCustomEditor : Editor {
 		fillWalls(mcd);
 	}
 
-	public override void OnInspectorGUI() {
+	public void OnGUI() {
         EditorGUI.BeginChangeCheck();
         Color defaultColor = GUI.backgroundColor;
 
         initializeGuiStyleColor();
 		if (colors == null) colors = new Colors();
 
-		MapCellData mcd = (MapCellData)target;
+		if (Selection.activeObject != null) mcd = (MapCellData)Selection.activeObject;
+        if (mcd == null) return;
 
         if (mcd.limites == null) mcd.limites = new Limite[4];
 		if (mcd.blueprint == null) {
 			mcd.blueprint = new Tile[mcd.dimension_x * mcd.dimension_y];
 			fillFloor(mcd);
 		}
-        //Centralizar norte/sul
-        GUIStyle centerStyle = new GUIStyle();
-		centerStyle.margin = new RectOffset(57,0,0,0);
 
-		EditorGUILayout.LabelField("Limites", EditorStyles.boldLabel);
-        GUILayout.BeginVertical("Box");
-        GUILayout.BeginVertical(centerStyle, GUILayout.Width(100));
-        mcd.limites[0] = (Limite) EditorGUILayout.EnumPopup(mcd.limites[0]);
-		GUILayout.EndVertical();
-		GUILayout.BeginVertical(GUILayout.Width(200));
-		GUILayout.BeginHorizontal();
-        mcd.limites[1] = (Limite)EditorGUILayout.EnumPopup(mcd.limites[1]);
-        mcd.limites[2] = (Limite)EditorGUILayout.EnumPopup(mcd.limites[2]);
-		GUILayout.EndVertical();
-        GUILayout.EndHorizontal();
-		GUILayout.BeginVertical(centerStyle, GUILayout.Width(100));
-        mcd.limites[3] = (Limite)EditorGUILayout.EnumPopup(mcd.limites[3]);
-        GUILayout.EndVertical();
-        GUILayout.EndVertical();
+        //Nome do objeto
+        GUIStyle title = new GUIStyle(EditorStyles.boldLabel);
+        title.fontSize = 20;
+        title.fixedHeight = 30;
+        EditorGUILayout.LabelField(mcd.name, title);
+        GUILayout.Space(8);
 
         //Botao de modificar dimensoes
-        EditorGUILayout.Space();
+        GUILayout.Space(8);
         EditorGUILayout.LabelField("Dimensões", EditorStyles.boldLabel);
         GUILayout.BeginVertical("Box", GUILayout.Width(200));
         GUILayout.BeginHorizontal();
@@ -204,63 +239,89 @@ public class MapCellDataCustomEditor : Editor {
         GUI.backgroundColor = defaultColor;
         GUILayout.EndHorizontal();
         GUILayout.EndVertical();
-        EditorGUILayout.Space();
+        GUILayout.Space(8);
+
+        //Centralizar norte/sul
+        GUIStyle centerStyle = new GUIStyle();
+        centerStyle.margin = new RectOffset(57, 0, 0, 0);
+
+        //Serializacao dos Limites
+        GUILayout.Space(8);
+        EditorGUILayout.LabelField("Limites", EditorStyles.boldLabel);
+        GUILayout.BeginVertical("Box");
+        GUILayout.BeginVertical(centerStyle, GUILayout.Width(100));
+        mcd.limites[0] = (Limite) EditorGUILayout.EnumPopup(mcd.limites[0]);
+		GUILayout.EndVertical();
+		GUILayout.BeginVertical(GUILayout.Width(200));
+		GUILayout.BeginHorizontal();
+        mcd.limites[1] = (Limite)EditorGUILayout.EnumPopup(mcd.limites[1]);
+        mcd.limites[2] = (Limite)EditorGUILayout.EnumPopup(mcd.limites[2]);
+		GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+		GUILayout.BeginVertical(centerStyle, GUILayout.Width(100));
+        mcd.limites[3] = (Limite)EditorGUILayout.EnumPopup(mcd.limites[3]);
+        GUILayout.EndVertical();
+        GUILayout.EndVertical();
 
         //Botao de Geracao de Paredes
-        EditorGUILayout.Space();
-		GUILayout.BeginVertical(GUILayout.Width(400));
+        GUILayout.Space(8);
+        GUILayout.BeginVertical(GUILayout.Width(400));
 		if (GUILayout.Button(new GUIContent("Generate walls and doors", "This will generate walls and doors based on the limits set. All tiles on the borders will be overwritten."), GUILayout.Height(50))) {
 			fillWalls(mcd);
         }
 		GUILayout.EndVertical();
-		EditorGUILayout.Space();
+        GUILayout.Space(8);
 
-		EditorGUILayout.LabelField("Blueprint", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Blueprint", EditorStyles.boldLabel);
 		GUILayout.BeginVertical("Box");
 
         //Centralizar indices
         centerStyle = new GUIStyle("Label");
         centerStyle.alignment = TextAnchor.LowerCenter;
 
+        //Matriz de tiles
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos,true, true, GUILayout.Width(Screen.width - 15), GUILayout.Height(Screen.height - 370));
         GUILayout.BeginHorizontal();
-        for (int j = 0; j < mcd.dimension_y; j++) GUILayout.Label(j.ToString("D2") + string.Empty, centerStyle);
+        GUILayout.Label(string.Empty, centerStyle, GUILayout.Width(55));
+        for (int j = 0; j < mcd.dimension_y; j++) GUILayout.Label(j + string.Empty, centerStyle, GUILayout.Width(55));
         GUILayout.EndHorizontal();
         for (int i = 0; i<mcd.dimension_x; i++) {
 			GUILayout.BeginHorizontal();
-            GUILayout.Label(i.ToString("D2") + string.Empty, centerStyle);
-			for (int j = 0; j<mcd.dimension_y; j++) {
+            GUILayout.Label(i + string.Empty, centerStyle, GUILayout.Width(55));
+            for (int j = 0; j<mcd.dimension_y; j++) {
                 switch (mcd.blueprint[mcd.accessMatrix(i, j)]) {
 					case Tile.VAZIO:
 						GUI.backgroundColor = colors.vazio;
-                        mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.ExpandHeight(true));
+                        mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.MinHeight(50), GUILayout.Width(55));
 						break;
 					case Tile.CHAO:
 						GUI.backgroundColor = colors.chao;
-						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.ExpandHeight(true));
-						break;
+						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.MinHeight(50), GUILayout.Width(55));
+                        break;
 					case Tile.PORTA:
 						GUI.backgroundColor = colors.porta;
-						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.ExpandHeight(true));
-						break;
+						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.MinHeight(50), GUILayout.Width(55));
+                        break;
 					case Tile.LAVA:
 						GUI.backgroundColor = colors.lava;
-						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.ExpandHeight(true));
-						break;
+						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.MinHeight(50), GUILayout.Width(55));
+                        break;
 					case Tile.ESPINHO:
 						GUI.backgroundColor = colors.espinho;
-						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.ExpandHeight(true));
-						break;
+						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.MinHeight(50), GUILayout.Width(55));
+                        break;
 					default:
 						GUI.backgroundColor = colors.parede;
-						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.ExpandHeight(true));
-						break;
+						mcd.blueprint[mcd.accessMatrix(i, j)] = (Tile)EditorGUILayout.EnumPopup(mcd.blueprint[mcd.accessMatrix(i, j)], guiStyleColor, GUILayout.MinHeight(50), GUILayout.Width(55));
+                        break;
                 }
             }
 			GUILayout.EndHorizontal();
         }
+            GUILayout.EndScrollView();
 
 		GUILayout.EndVertical();
-        
+
         if (EditorGUI.EndChangeCheck()) {
             EditorUtility.SetDirty(mcd);
             //Undo.RecordObject(mcd, "Modified Map Cell \"" + mcd.name + "\"");
