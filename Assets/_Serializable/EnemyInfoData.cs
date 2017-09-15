ef116cf40381d399aaa1520b7c22ffbd12113683
Jerry123i <igorsc.jerry@gmail.com>
2017-09-15 17:29:08 -0300

    Comit pq me expulsaram da sala

---
+++ Assets/_Serializable/EnemyInfoData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Flags]
+public enum EnemyGroup {
+        FIRE   = 1,
+        EARTH  = 2,
+        WATER  = 4,
+        AIR    = 8,
+        AETHER = 16
+}
+
+[System.Flags]
+public enum EnemyTag {
+    RANGED = 1,
+    MELEE = 2,
+    SUPPORT = 4,
+    IMMOVABLE = 8,
+    VANILLA = 16,
+    FODDER= 32
+}
+
+[CreateAssetMenu(fileName = "EnemyTag", menuName = "Enemy Tag", order = 2)]
+public class EnemyInfoData : ScriptableObject {
+    
+    public EnemyGroup groups;
+    public EnemyTag tags;
+    
+
+}
